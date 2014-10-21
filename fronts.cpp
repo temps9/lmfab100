ba160f504f08db7f662652e13c13e0e7f7908c05
André pierre <temps.jo@gmail.com>
2014-10-21 22:35:42 +0200

    Create fronts.cpp

---
+++ fronts.cpp
@@ -0,0 +1,236 @@
+#include "fronts.h"
+#include "jo.h"
+
+jo clavier32[26] = {
+   { // a touche1
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 58}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // b touche25
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 178}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+
+   { // c touche23
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 168}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // d touche13
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 118}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // e touche3
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 68}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // f touche14
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 123}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // g touche15
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 128}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // h touche16
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 133}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // i touche8
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 93}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // j touche17
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 138}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // k touche18
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 143}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // l touche19
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 148}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // m touche20
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 153}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // n touche26
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 183}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+
+   { // o touche9
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 98}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+
+   { // p touche10
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 103}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // q touche11
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 108}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // r touche4
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 73}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // s touche12
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 113}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // t touche5
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 78}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // u touche7    
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 88}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // v touche24
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 173}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // w touche21
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 158}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+   { // x touche22
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 163}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // y touche6    
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 83}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   },
+
+
+   { // z touche2   
+    100, // laforceh
+    {129, 128}, // amplitude
+    {12, 63}, // dureefront
+    {1, 1}, // varamplitude
+    {0, 0} // vardureefront 
+   }
+};
+
+jo *mesjo = clavier32;
