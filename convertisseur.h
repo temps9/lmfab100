a07bb9ccf11fc86a52f24b7e6197e67fe5232ca6
André pierre <temps.jo@gmail.com>
2014-10-21 22:34:51 +0200

    Create convertisseur.h

---
+++ convertisseur.h
@@ -0,0 +1,12 @@
+#ifndef _convertisseur_h_
+#define _convertisseur_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "jo.h"
+#include "fronts.h"
+
+void genereFichierJo(char c);
+void conversionJoWav(char c, int n);
+
+#endif // _convertisseur_h_
